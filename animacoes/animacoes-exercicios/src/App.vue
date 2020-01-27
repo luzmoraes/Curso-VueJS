b4d1536f78e6e3bfed17d9c43d142b68f8bdefad
Anderson Moraes <anderson@ycloud.com.br>
2020-01-27 16:32:15 -0300

    modulo animação e transição finalizado

--- animacoes/animacoes-exercicios/src/App.vue
+++ animacoes/animacoes-exercicios/src/App.vue
@@ -1,5 +1,6 @@
 <template>
 	<div id="app" class="container-fluid">
+		<!--
 		<h1>Animações</h1>
 		<hr>
 		<b-button variant="primary" class="mb-4"
@@ -16,12 +17,15 @@
 		<transition name="slide">
 			<b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
 		</transition>
+		-->
 		<!-- appear - aplica a animação no carregamento da aplicação -->
+		<!--
 		<transition name="fade-slide" type="animation" appear>
 			<b-alert variant="info" show v-if="exibir">{{ msg }}</b-alert>
 		</transition>
-
+		-->
 		<!-- Nesse exemplo não vamos usar o padrão de animação do vue, vamos usar o Animate.css (importado na index) -->
+		<!--
 		<transition
 			enter-active-class="animated bounce"
 			leave-active-class="animated shake">
@@ -29,28 +33,127 @@
 		</transition>
 
 		<hr>
+
 		<b-select v-model="tipoAnimacao" class="mb-4">
 			<option value="fade">Fade</option>
 			<option value="slide">Slide</option>
 		</b-select>
 
 		<transition :name="tipoAnimacao">
-			<b-alert variant="danger" show v-if="exibir">{{ msg }}</b-alert>
+			<b-alert variant="danger" show v-show="exibir">{{ msg }}</b-alert>
 		</transition>
+
+		<transition :name="tipoAnimacao" mode="out-in">
+			<b-alert variant="info" show v-if="exibir" key="info">{{ msg }}</b-alert>
+			<b-alert variant="warning" show v-else key="warn">{{ msg }}</b-alert>
+		</transition>
+		-->
+
+		<!-- Animation with Javascript -->
+		<!--
+		<hr>
+		<button @click="exibir2 = !exibir2">Alternar</button>
+		<transition
+			:css="false"
+			@before-enter="beforeEnter"
+			@enter="enter"
+			@after-enter="afterEnter"
+			@enter-cancelled="enterCancelled"
+			@before-leave="beforeLeave"
+			@leave="leave"
+			@after-leave="afterLeave"
+			@leave-cancelled="leaveCancelled">
+			<div class="caixa" v-if="exibir2"></div>
+		</transition>
+		
+		<hr>
+		<div class="mb-4">
+			<b-button variant="primary" class="mr-2"
+				@click="componenteSelecionado='AlertaInfo'">Info</b-button>
+			<b-button variant="secondary" class="mr-2"
+				@click="componenteSelecionado='AlertaAdvertencia'">Advertência</b-button>
+		</div>
+		<transition name="fade" mode="out-in">
+			<component :is="componenteSelecionado"></component>
+		</transition>
+		-->
+
+		<hr>
+		<b-button @click="adiconarAluno" class="mb-4">Adicionar Aluno</b-button>
+		<transition-group name="fade-slide" tag="div"> <!-- por default envolve com a tag span, mas podemos setar como no exemplo-->
+			<b-list-group v-for="(aluno, i) in alunos" :key="aluno">
+				<b-list-group-item @click="removerAluno(i)">{{ aluno }}</b-list-group-item>
+			</b-list-group>
+		</transition-group>
 
 	</div>
 </template>
 
 <script>
+import AlertaAdvertencia from './AlertaAdvertencia'
+import AlertaInfo from './AlertaInfo'
 
 export default {
+	components: { AlertaAdvertencia, AlertaInfo },
 	data() {
 		return {
+			alunos: ['Roberto', 'Julia', 'Tereza', 'Paulo'],
 			msg: 'Uma mensagem de informação para o usuário!',
 			exibir: false,
-			tipoAnimacao: 'fade'
+			exibir2: true,
+			tipoAnimacao: 'fade',
+			larguraBase: 0,
+			componenteSelecionado: 'AlertaInfo'
 		}
-	}
+	},
+	methods: {
+		adiconarAluno() {
+			const s = Math.random().toString(36).substring(2)
+			this.alunos.push(s)
+		},
+		removerAluno(indice) {
+			this.alunos.splice(indice, 1)
+		},
+		animar(el, done, negativo) {
+			let rodada = 1
+			const temporizador = setInterval(() => {
+				const novaLargura = this.larguraBase +
+					(negativo ? -rodada * 10 : rodada * 10)
+				el.style.width = `${novaLargura}px`
+				rodada++
+				if (rodada > 30) {
+					clearInterval(temporizador)
+					done()
+				}
+			}, 20)
+		},
+		beforeEnter(el) {
+			this.larguraBase = 0
+			el.style.width = `${this.larguraBase}px`
+		},
+		enter(el, done) {
+			this.animar(el, done, false)
+		},
+		afterEnter(el) {
+			
+		},
+		enterCancelled() {
+			
+		},
+		beforeLeave(el) {
+			this.larguraBase = 300
+			el.style.width = `${this.larguraBase}px`
+		},
+		leave(el, done) {
+			this.animar(el, done, true)
+		},
+		afterLeave(el) {
+			
+		},
+		leaveCancelled() {
+			
+		}
+	},
 }
 </script>
 
@@ -63,6 +166,13 @@ export default {
 	color: #2c3e50;
 	margin-top: 60px;
 	font-size: 1.5rem;
+}
+
+.caixa {
+	height: 100px;
+	width: 300px;
+	margin: 30px auto;
+	background-color: lightgreen;
 }
 
 /* fade */
@@ -112,6 +222,8 @@ export default {
 }
 
 .fade-slide-leave-active {
+	position: absolute;
+	width: 100%;
 	animation: fade-slide-out 2s ease;
 	transition: opacity 2s;
 }
@@ -120,5 +232,8 @@ export default {
 	opacity: 0;
 }
 
+.fade-slide-move {
+	transition: transform 1s;
+}
 
 </style>
