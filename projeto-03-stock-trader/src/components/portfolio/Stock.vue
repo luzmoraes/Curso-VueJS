b42b93c9b0c85af87357ff8a58c328cb687e94d4
Anderson Moraes <anderson@ycloud.com.br>
2020-02-03 21:23:40 -0300

    projeto 03 em andamento, aula 306 finalizada

--- projeto-03-stock-trader/src/components/portfolio/Stock.vue
+++ projeto-03-stock-trader/src/components/portfolio/Stock.vue
@@ -1,10 +1,59 @@
 <template>
-    <h1>Ação (Portfólio)</h1>
+    <v-flex class="pr-3 pb-3" xs12 md6 lg4>
+        <v-card class="blue darken-3 white--text">
+            <v-card-title class="headline">
+                <strong>
+                        {{ stock.name }} 
+                        <small>
+                            (Preço: {{ stock.price }} | Qtd: {{ stock.quantity }})
+                        </small>
+                    </strong>
+            </v-card-title>
+        </v-card>
+        <v-card>
+            <v-container fill-height>
+                <v-text-field label="Quantidade" type="number"
+                    v-model.number="quantity" />
+                <v-btn class="blue darken-3 white--text"
+                    :disabled="quantity <= 0 || !Number.isInteger(quantity)"
+                    @click="sellStock">Vender</v-btn>
+            </v-container>
+        </v-card>
+    </v-flex>
 </template>
 
 <script>
-export default {
+/* Nesse componente vamos chamar a action com o mapAction */
+import { mapActions } from 'vuex'
 
+export default {
+    props: [ 'stock' ],
+    data() {
+        return {
+            quantity: 0
+        }
+    },
+    methods: {
+        /*
+        * não podemos chamar dessa maneira pq já tem um método com esse nome e daria
+        * conflito, por isso temos que atribuir outro nome 'sellStockActions'
+        ...mapActions(['sellStock']),
+        */
+        ...mapActions({ sellStockActions: 'sellStock' }),
+        sellStock() {
+            const order = {
+                stockId: this.stock.id,
+                stockPrice: this.stock.price,
+                quantity: this.quantity
+            }
+            /* E com o mapActions, ao invés disso: */
+            // this.$store.dispatch('sellStock', order)
+            /* Chamamos a ação assim: */
+            this.sellStockActions(order)
+
+            this.quantity = 0
+        }
+    },
 }
 </script>
 
---
+++ projeto-03-stock-trader/src/store/modules/stocks.js
@@ -0,0 +1,25 @@
+import stocks from '@/data/stocks'
+
+export default {
+    state: {
+        stocks: []
+    },
+    mutations: {
+        setStocks(state, payload) {
+            state.stocks = stocks
+        }
+    },
+    actions: {
+        buyStock({ commit }, order) {
+            commit('buyStock', order)
+        },
+        initStocks({ commit }) {
+            commit('setStocks', stocks)
+        }
+    },
+    getters: {
+        stocks(state) {
+            return state.stocks
+        }
+    }
+}
